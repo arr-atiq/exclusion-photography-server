8ed2bec67cf5050404072af8f70c5886f39296e7
aar-atiq <aar.msdeveloper01@gmail.com>
2021-04-18 17:40:00 +0600

    created orderPlace and allPlaceOrders api

--- index.js
+++ index.js
@@ -23,59 +23,88 @@ app.get('/', (req, res) => {
 
 
 client.connect(err => {
-  const PhotographyCollection = client.db("exclusionsPhotography").collection("photography");
-  const ReviewCollection = client.db("exclusionsPhotography").collection("placeReview");
-  const PhotographerCollection = client.db("exclusionsPhotography").collection("photographerCollection");
- 
-  app.get('/allServices', (req, res) => {
-    PhotographyCollection.find()
-    .toArray((err, items) => {
-      res.send(items)
+    const PhotographyCollection = client.db("exclusionsPhotography").collection("photography");
+    const ReviewCollection = client.db("exclusionsPhotography").collection("placeReview");
+    const PhotographerCollection = client.db("exclusionsPhotography").collection("photographerCollection");
+    const OrderCollection = client.db("exclusionsPhotography").collection("placeOrder");
+
+    app.get('/allServices', (req, res) => {
+        PhotographyCollection.find()
+            .toArray((err, items) => {
+                res.send(items)
+            })
     })
-  })
 
-  app.post('/addService', (req, res) => {
-    const newService = req.body;
-    PhotographyCollection.insertOne(newService)
-    .then(result => {
-      console.log(result);
-      res.redirect('/')
+    app.post('/addService', (req, res) => {
+        const newService = req.body;
+        PhotographyCollection.insertOne(newService)
+            .then(result => {
+                console.log(result);
+                res.redirect('/')
+            })
     })
-  })
 
 
-  app.get('/allReviews', (req, res) => {
-    ReviewCollection.find()
-    .toArray((err, items) => {
-      res.send(items)
+    app.get('/allReviews', (req, res) => {
+        ReviewCollection.find()
+            .toArray((err, items) => {
+                res.send(items)
+            })
     })
-  })
 
-  app.post('/addReview', (req, res) => {
-    const newReview = req.body;
-    ReviewCollection.insertOne(newReview)
-    .then(result => {
-      console.log(result);
-      res.redirect('/')
+    app.post('/addReview', (req, res) => {
+        const newReview = req.body;
+        ReviewCollection.insertOne(newReview)
+            .then(result => {
+                console.log(result);
+                res.redirect('/')
+            })
     })
-  })
 
-  app.post('/makeAdmin', (req, res) => {
-    const newReview = req.body;
-    PhotographerCollection.insertOne(newReview)
-    .then(result => {
-      console.log(result);
-      res.redirect('/')
+    app.post('/makeAdmin', (req, res) => {
+        const newReview = req.body;
+        PhotographerCollection.insertOne(newReview)
+            .then(result => {
+                console.log(result);
+                res.redirect('/')
+            })
     })
-  })
 
-  app.post('/isPhotographer', (req, res) => {
-    const email = req.body.email;
-    PhotographerCollection.find({ email: email })
-        .toArray((err, doctors) => {
-            res.send(doctors.length > 0);
+    app.get('/photographers', (req, res) => {
+        PhotographerCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
+
+    app.post('/isPhotographer', (req, res) => {
+        const email = req.body.email;
+        PhotographerCollection.find({ email: email })
+            .toArray((err, result) => {
+                res.send(result.length > 0);
+            })
+    })
+
+    app.get('/ServiceProduct/:id', (req, res) => {
+        PhotographyCollection.find({_id: ObjectId(req.params.id)})
+        .toArray((err, items) => {
+          res.send(items[0]);
         })
-})
+      })
+
+      app.post('/orderPlace', (req, res) =>{
+        const newOrder = req.body;
+        OrderCollection.insertOne(newOrder)
+        .then(result => {
+          res.redirect('/')
+        })
+      })
+      app.get('/allPlaceOrders', (req, res) => {
+        OrderCollection.find()
+        .toArray((err, items) => {
+          res.send(items)
+        })
+      })
 
 });
 
